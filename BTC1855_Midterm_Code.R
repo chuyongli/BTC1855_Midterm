1441c0d6e22e7a667f01489823899a2a4a4c5ca6
Trinley Palmo <t.palmo@hotmail.com>
2024-07-30 20:26:23 -0400

    Fix structural errors in stations dataset

--- BTC1855_Midterm_Code.R
+++ BTC1855_Midterm_Code.R
@@ -1,10 +1,22 @@
 # BTC1855 - Midterm
 # By Trinley Palmo
 
+# Libraries needed
+library(lubridate)
+
 # Set working directory foro where to find the data files
 setwd("C://Users/tpalm/Desktop/MY FILES/UofT/MBiotech/BTC1855/babs")
 
-# Read the data files and save them to objects
+# Read the data files and save them to objects.
 stations <- read.csv("station.csv")
 weather <- read.csv("weather.csv")
 trips <- read.csv("trip.csv")
+
+# First, work with the `stations` dataset.
+# Explore the `stations` dataset.
+dim(stations)
+str(stations)
+summary(stations)
+
+# Convert installation date to datetime objects.
+stations$installation_date <- mdy(stations$installation_date)
